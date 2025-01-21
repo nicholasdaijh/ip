959ab130b7a6e8bca628b1042106c24a2aa9fb4d
nicholasdaijh <nicholasdaijh@gmail.com>
2025-01-21 19:43:39 +0800

    added scanner to echo user's input

--- src/main/java/Nicholas.java
+++ src/main/java/Nicholas.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Nicholas {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Nicholas!");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________\n");
+        //Scanner to receive user's input
+        Scanner scanner = new Scanner(System.in);
+        //store user's input
+        String userInput = scanner.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________\n");
+            userInput = scanner.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________\n");
+        System.out.println("____________________________________________________________");
     }
 }
