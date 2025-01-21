26ee1743754fd37c228b75730412f90bfc31f2bd
nicholasdaijh <nicholasdaijh@gmail.com>
2025-01-21 20:10:03 +0800

    Add, list

--- src/main/java/Nicholas.java
+++ src/main/java/Nicholas.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Nicholas {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Nicholas!");
         System.out.println("What can I do for you?");
@@ -11,9 +13,21 @@ public class Nicholas {
         //store user's input
         String userInput = scanner.nextLine();
         while (!userInput.equalsIgnoreCase("bye")) {
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+                userInput = scanner.nextLine();
+                continue;
+            }
             System.out.println("____________________________________________________________");
-            System.out.println(userInput);
+            System.out.println("added: " + userInput);
             System.out.println("____________________________________________________________\n");
+            tasks[taskCount] = userInput;
+            taskCount++;
             userInput = scanner.nextLine();
         }
         System.out.println("____________________________________________________________");
