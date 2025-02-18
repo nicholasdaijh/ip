9d3d6d86d8437e4870b517a3f953c92dadcb7252
nicholasdaijh <nicholasdaijh@gmail.com>
2025-02-18 16:25:49 +0800

    Add ui.png and user guide

--- src/main/java/nicholas/gui/Main.java
+++ src/main/java/nicholas/gui/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setNicholas(nicholas); // Inject the Nicholas instance
-            stage.setTitle("Nicholas Chatbot");
+            stage.setTitle("Nicholas");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
